4fe9c5094fe5ea8b1aa7b73ae2936d57aef6f499
Justin Lettau <justin.b.lettau@gmail.com>
2018-10-16 18:43:53 -0400

    Updates

---
+++ precache-manifest.79ef42e24f27359a16f320ba5a700ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9610cefc78a55e08cdd3",
+    "url": "/npm-dashboard/static/js/runtime~main.9610cefc.js"
+  },
+  {
+    "revision": "c1074badd280f83bd761",
+    "url": "/npm-dashboard/static/js/main.c1074bad.chunk.js"
+  },
+  {
+    "revision": "4e6e1c6c857ac1a06f70",
+    "url": "/npm-dashboard/static/js/1.4e6e1c6c.chunk.js"
+  },
+  {
+    "revision": "c1074badd280f83bd761",
+    "url": "/npm-dashboard/static/css/main.e0bd6d43.chunk.css"
+  },
+  {
+    "revision": "4e6e1c6c857ac1a06f70",
+    "url": "/npm-dashboard/static/css/1.5a01c377.chunk.css"
+  },
+  {
+    "revision": "d1421d09a5b951e933a36c0a96b67bcf",
+    "url": "/npm-dashboard/index.html"
+  }
+];
